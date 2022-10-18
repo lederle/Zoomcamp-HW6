98ba509a045c879c652fab93b0fa3b0ab2ae4403
mike lederle <mrlederle@gmail.com>
2022-10-18 13:04:38 -0400

    add save_fig util

--- hw6.py
+++ hw6.py
@@ -9,3 +9,13 @@ def load_housing_data():
         url = "https://raw.githubusercontent.com/alexeygrigorev/datasets/master/housing.csv"
         urllib.request.urlretrieve(url, csv_path)
     return pd.read_csv(Path("datasets/housing.csv"))
+
+IMAGES_PATH = Path() / "images"
+IMAGES_PATH.mkdir(parents = True, exist_ok = True)
+
+def save_fig(fig_id, tight_layout = True, fig_extension = "png", resolution = 300):
+    path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
+    if tight_layout:
+        plt.tight_layout()
+        plt.savefig(path, format = fig_extension, dpi = resolution)
+
