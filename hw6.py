6612dd34f9c8992f566cd202ff0c236375d616e3
mike lederle <mrlederle@gmail.com>
2022-10-18 13:43:40 -0400

    plot log transform of median house price

--- hw6.py
+++ hw6.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import pandas as pd
 import urllib.request
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 def load_housing_data():
     csv_path = Path("datasets/housing.csv")
     if not csv_path.is_file():
@@ -18,4 +21,14 @@ def save_fig(fig_id, tight_layout = True, fig_extension = "png", resolution = 30
     if tight_layout:
         plt.tight_layout()
         plt.savefig(path, format = fig_extension, dpi = resolution)
+
+def plot_log_transform_of_median_house_price(df):
+    fig, axs = plt.subplots(1, 2, figsize = (8, 3), sharey = True)
+    df["median_house_value"].hist(ax = axs[0], bins = 50)
+    df["median_house_value"].apply(np.log).hist(ax = axs[1], bins = 50)
+    axs[0].set_ylabel("Number of districts")
+    axs[0].set_xlabel("Median House Value")
+    axs[1].set_xlabel("Log of Median House Value")
+    save_fig("log_transform_median_house_price")
+    plt.show()
 
